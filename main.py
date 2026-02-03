2bff2bdc7e454d74a9c5137e80c1a73d5b891a27
George <mironenkogeorge84@gmail.com>
2026-02-03 15:08:40 +0300

    Создания файла main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+def print_hi(name: str = 'PyCharm'):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('Георгий')
+    print_hi('GUAP')
+
+    text = "*" * 12
+    print_hi('PyCharm')
+    print_hi(text)
