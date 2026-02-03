451c6e8fcedae2326cdde1dad06785b86ac38848
George <mironenkogeorge84@gmail.com>
2026-02-03 15:31:08 +0300

    Добавление функции generator_email для генерации email

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from random import randint, choices
+import string
+
+
 def print_hi(name: str = 'PyCharm'):
     print(f'Hi, {name}')
+
+# функция для генерации email
+def generator_email(user_name: str = None) -> str:
+    if user_name is None:
+        user_name = ''.join(choices(string.ascii_letters, k=10, ))
+
+    return f'{user_name}{randint(10, 999)}@gmail.com'
+
 
 
 if __name__ == '__main__':
